ac14925b3ea421f3292fd4dbd9ea24fed385e4c1
Koen <>
2020-04-14 21:00:16 +0200

    useInput result now has setValue, which allows the value to be set directly

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,23 +1,31 @@
-import {BaseSyntheticEvent, useCallback, useMemo, useState} from "react";
+import {BaseSyntheticEvent, Dispatch, SetStateAction, useCallback, useMemo, useState} from "react";
 
 export type OnChangeHandler = (e: BaseSyntheticEvent) => void;
 export type UseInput = {
 	defaultValue: string,
 	value: string,
 	clear: () => void,
+	setValue: Dispatch<SetStateAction<string>>,
 	onChange: OnChangeHandler,
 	bind: {
-		defaultValue: string,
+		defaultValue?: string,
 		onChange: OnChangeHandler,
+		value?: string,
 	}
 };
 
 export function useInput(defaultValue: string = ""): UseInput {
 	const [value, setValue] = useState<string>(defaultValue);
+	let callback: CallableFunction | undefined = undefined;
 
 	const onChange = (e: BaseSyntheticEvent) => {
 		e.persist();
-		setValue(() => e.target.value);
+		let newValue = e.target.value;
+
+		setValue(() => newValue);
+		if (callback) {
+			callback(newValue);
+		}
 	};
 
 	const clear = useCallback(() => setValue(defaultValue), []);
@@ -26,11 +34,12 @@ export function useInput(defaultValue: string = ""): UseInput {
 		defaultValue,
 		value,
 		clear,
+		setValue,
 		onChange,
 
 		bind: {
-			defaultValue,
 			onChange,
+			value,
 		},
 	}), [defaultValue, value, clear]);
 }
