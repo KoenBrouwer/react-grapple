2a7bd294f5a332ad0ca7bcb0fcd65c9aa6f3de2c
Koen <>
2020-08-29 18:08:15 +0200

    Added useNumberinput for input[type=number] with min, max and step options.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import useToggle from "./hooks/useToggle";
 import useConditionalByTime from "./hooks/useConditionalByTime";
 import Conditional from "./Conditional";
 import useInput, {Validators} from "./hooks/useInput";
+import useNumberInput from "./hooks/useNumberInput";
 
 export {
 	useInput,
+	useNumberInput,
 	useToggle,
 	useConditionalByTime,
 	Conditional,
