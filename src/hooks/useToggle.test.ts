cc80af4328ad0a78e3bd761182e978d7d4e4a902
Koen <>
2020-08-31 16:35:31 +0200

    Added test for useToggle hook.

---
+++ src/hooks/useToggle.test.ts
@@ -0,0 +1,59 @@
+import {act, renderHook} from '@testing-library/react-hooks'
+import useToggle from "./useToggle";
+
+test('Should initiate with false as default value', () => {
+	const {result} = renderHook(() => useToggle());
+	const [state] = result.current;
+	expect(state).toBe(false);
+})
+
+test('Should initiate with provided value true', () => {
+	const {result} = renderHook(() => useToggle(true));
+	const [state] = result.current;
+	expect(state).toBe(true);
+})
+
+test('Should initiate with provided value false', () => {
+	const {result} = renderHook(() => useToggle(false));
+	const [state] = result.current;
+	expect(state).toBe(false);
+})
+
+test('Should toggle from false to true', () => {
+	const {result} = renderHook(() => useToggle(false));
+
+	let [state, toggleState] = result.current;
+
+	act(() => {
+		toggleState();
+	});
+
+	[state] = result.current;
+	expect(state).toBe(true);
+})
+
+test('Should toggle from true to false explicitly', () => {
+	const {result} = renderHook(() => useToggle(true));
+
+	let [state, toggleState] = result.current;
+
+	act(() => {
+		toggleState(false);
+	});
+
+	[state] = result.current;
+	expect(state).toBe(false);
+})
+
+test('Should toggle from false to true explicitly', () => {
+	const {result} = renderHook(() => useToggle(false));
+
+	let [state, toggleState] = result.current;
+
+	act(() => {
+		toggleState(true);
+	});
+
+	[state] = result.current;
+	expect(state).toBe(true);
+})
