7a21ec8d5bbfbaa6650a1a94fd993b8f656482ee
Koen <>
2020-09-13 17:06:06 +0200

    Both new hooks are now exported

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import useConditionalByTime from "./hooks/useConditionalByTime";
 import Conditional from "./Conditional";
 import useInput, {Validators} from "./hooks/useInput";
 import useNumberInput from "./hooks/useNumberInput";
+import useIsMobile, { useIsMobileOnce } from "./hooks/useIsMobile";
 
 export {
 	useInput,
 	useNumberInput,
 	useToggle,
 	useConditionalByTime,
+	useIsMobile,
+	useIsMobileOnce,
 	Conditional,
 	Validators,
 }
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -6,6 +6,8 @@ test("exports all the correct things", () => {
 	expect(Grapple).toHaveProperty("useNumberInput");
 	expect(Grapple).toHaveProperty("useToggle");
 	expect(Grapple).toHaveProperty("useConditionalByTime");
+	expect(Grapple).toHaveProperty("useIsMobile");
+	expect(Grapple).toHaveProperty("useIsMobileOnce");
 	expect(Grapple).toHaveProperty("Conditional");
 	expect(Grapple).toHaveProperty("Validators");
 
